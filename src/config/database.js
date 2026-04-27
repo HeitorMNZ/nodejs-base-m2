277738462b42e17e7a05fee051eae2cbfb2d7a34
Heitor_Martins_Nogueira <heitormartinsnogueira2008@gmail.com>
2026-04-27 08:43:45 -0300

    fiz tudo ai

---
+++ src/config/database.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "username": "postgres",
+  "password": null,
+  "database": "projetosenai",
+  "host": "127.0.0.1",
+  dialect: "mysql",
+};
--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 
 const routes = Router();
 
-routes.get("/health", (req, res) => {
+routes.get("/", (req, res) => {
   return res.status(200).json({ message: "Server on" });
 });
 
